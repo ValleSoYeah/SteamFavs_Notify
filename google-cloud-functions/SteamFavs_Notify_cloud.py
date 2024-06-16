05983ff7a1086c5438f826d5dadc28673c90fa2c
Valentin Schoop <107258267+ValleSoYeah@users.noreply.github.com>
2024-06-16 13:24:49 +0200

    Update SteamFavs_Notify_cloud.py

--- google-cloud-functions/SteamFavs_Notify_cloud.py
+++ google-cloud-functions/SteamFavs_Notify_cloud.py
@@ -1,5 +1,6 @@
 import base64
 import os
+from google.oauth2.credentials import Credentials
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 import pandas as pd
@@ -9,11 +10,6 @@ import requests
 import pytz
 from datetime import datetime
 
-#########################################################################################################
-# to run in Cloud Functions, all credentials need to be stored as environment variables with these names:
-# my_APIkey, my_bot_token, my_telegram_id, my_spreadsheet_id, serv_acc
-##########################################################################################################
-
 
 def activate_fav_notify_sheets(event, context):
     """Triggered from a message on a Cloud Pub/Sub topic.
@@ -21,11 +17,16 @@ def activate_fav_notify_sheets(event, context):
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
+    verbose = 1
     try:
         pubsub_message = base64.b64decode(event['data']).decode('utf-8')
         print(f"Triggered by the Pub/Sub message: '{pubsub_message}'")
+        if pubsub_message == "Hello World":
+            verbose =2
+            print("Assuming Test-Mode, verbosity set to 2")
     except:
         print("Triggered by something other than a Pub/Sub message.")
+
 
     print(f"Script version number {os.environ['K_REVISION']}")
 
@@ -37,8 +38,10 @@ def activate_fav_notify_sheets(event, context):
     my_APIkey = os.environ["my_APIkey"]
     my_bot_token = os.environ["my_bot_token"]
     my_telegram_id = os.environ["my_telegram_id"]
+    my_telegram_group_id = os.environ["my_telegram_group_id"]
     my_spreadsheet_id = os.environ["sheet_id"]
     serv_acc = eval(os.environ["appspot_service_acc"])
+
 
     
     #loading favlist from sheet
@@ -48,25 +51,36 @@ def activate_fav_notify_sheets(event, context):
     var = service.spreadsheets().values().get(spreadsheetId=my_spreadsheet_id, range="my_favs!"+RANGE).execute()
     var = var.get('values', [])
     if not var:
-        print('No data found.')
+        if verbose > 0:
+            print('No data found.')
         return
     else:    
         my_favs= pd.DataFrame(var[1:], columns=var[0])
-        print('Favs imported from GSheets!')
+        if verbose > 0:
+            print('Favs imported from GSheets!', my_favs)
 
     #Loading last state of running games from chached sheet
     var = service.spreadsheets().values().get(spreadsheetId=my_spreadsheet_id,range="cache!"+RANGE).execute()
     var = var.get('values', [])
+    print(var)
     my_notify = {}
+    id_name_map = {}
+    for i in range(len(var)):
+            id_name_map[my_favs.iloc[i]["steamid"]] = my_favs.iloc[i]["friend"]
     # reset chached state at first invocation of the day (after 18:00)
-    if not var or datetime.now(pytz.timezone('Europe/Berlin')).strftime('%H:%M')[:4] == "18:0":
+    if not var or datetime.now(pytz.timezone('Europe/Berlin')).strftime('%H:%M')[:4] in ["18:0","10:0"]:
         for i in range(my_favs.shape[0]):
-            my_notify[my_favs.iloc[i]["steamname"]]="-"
-        print("State dictionary was reset")
+            my_notify[my_favs.iloc[i]["steamid"]]="-"
+        if verbose > 0:
+            print("State dictionary was reset")
     else:
         for i in range(len(var)):
             my_notify[var[i][0]] = var[i][1]
-        print("Cached state imported from GSheets")
+        if verbose > 0:
+            print("Cached state imported from GSheets")
+    if verbose > 1:
+        print("my_notify: ", my_notify)
+        print("id_name_map: ", id_name_map)
     
 
     def check_for_matches(favs, APIkey):
@@ -82,20 +96,32 @@ def activate_fav_notify_sheets(event, context):
             try:
                 friendstatus = friendstatus["response"]["players"][0] #fails if profile is not accessible
                 friend_name = friendstatus["personaname"]
+                if verbose > 1:
+                    print(f"Checking status for: {friend_name}")
+                    print("Favorite Games:", fav_games)
                 try:
                     curr_game = friendstatus["gameextrainfo"] #fails if friend is not currently playing a game
-                    if curr_game in fav_games: 
-                        matches[friend_name] = curr_game
+                    if verbose > 1:
+                        print("Currently playing:", curr_game)
+                    if (curr_game in fav_games) or (fav_games == ["*"]): 
+                        matches[id] = curr_game
                 except: 
-                    matches[friend_name] = "-"
+                    if verbose > 1:
+                        print(f"{friend_name} is ot playing anything")
+                    matches[id] = "-"
             except:
                 continue
+        if verbose > 1:
+            print("Matches: ", matches)
         return matches 
 
     def check_for_change(matches):
         """Checks whether any detected matches are new or reoccuring"""
         has_changed = False
         for entry in matches.keys():
+            if verbose > 1:
+                print("my_notify[entry]", entry, my_notify[entry])
+                print("matches[entry]", matches[entry])
             if my_notify[entry] != matches[entry]:
                 my_notify[entry] = matches[entry]
                 if my_notify[entry] != "-":
@@ -106,7 +132,8 @@ def activate_fav_notify_sheets(event, context):
         """Sends a message via the specified bot, to the specified telegram ID"""
         url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={telegram_id}&text={message}"
         requests.get(url).json()
-        print(f"{datetime.now(pytz.timezone('Europe/Berlin')).strftime('%H:%M:%S')}: Just sent this message via Telegram: '{message}'")
+        if verbose > 0:
+            print(f"{datetime.now(pytz.timezone('Europe/Berlin')).strftime('%H:%M:%S')}: Just sent this message via Telegram: '{message}'")
 
      
     #Run code
@@ -115,10 +142,12 @@ def activate_fav_notify_sheets(event, context):
     if has_changed == True:
         for i in my_notify.keys():
             if my_notify[i] != "-":
-                message = f"{i} is now playing {my_notify[i]}!"
+                message = f"{id_name_map[i]} is now playing {my_notify[i]}!"
                 send_message(message, my_bot_token, my_telegram_id)
+                send_message(message, my_bot_token, my_telegram_group_id)
     else:
-        print(f"{datetime.now(pytz.timezone('Europe/Berlin')).strftime('%H:%M:%S')} Nothing to report")
+        if verbose > 0:
+            print(f"{datetime.now(pytz.timezone('Europe/Berlin')).strftime('%H:%M:%S')} Nothing to report")
     
     #cache current state
     var=[]
